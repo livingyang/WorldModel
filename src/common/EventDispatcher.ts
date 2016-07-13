d722d9b3071a86d870343f9f335c6567fe0c4931
huanming li <lhm.cold@163.com>
2016-07-13 11:47:30 +0800

    Refactor: EventDispatcher.ts

--- src/common/EventDispatcher.ts
+++ src/common/EventDispatcher.ts
@@ -2,15 +2,29 @@
 // class EventDispatcher
 // 事件处理类
 // ###
+
+interface Invoker {
+    target;
+    handler: Function;
+}
+
 export class EventDispatcher {
-    _oneEventInvokerMap = {};
-    _eventInvokerMap = {};
+    _eventInvokerMap: {[id: string]: Invoker[]} = {};
+    _oneEventInvokerMap: {[id: string]: Function[]} = {};
+
+    getInvokerList(event: string) {
+        return this._eventInvokerMap[event] || [];
+    }
+
+    getOneInvokerList(event: string) {
+        return this._oneEventInvokerMap[event] || [];
+    }
 
     // # 注册事件
     // # event: 事件名称
     // # target: 绑定回调函数中的 this 为 target
     // # handler: 回调函数, 当handler为空时，默认 handler = target[event]
-    on(event: string, target, handler = target[event]) {
+    on(event: string, target, handler: Function = target[event]) {
         if (this._eventInvokerMap[event] == null) {
             this._eventInvokerMap[event] = [];
         }
@@ -21,20 +35,20 @@ export class EventDispatcher {
     }
 
     // # 注销事件
-    off(event: string, target, handler = target[event]) {
+    off(event: string, target, handler: Function = target[event]) {
         if (this._eventInvokerMap[event] != null) {
-            let newEventMap = [];
-            for (let invoker of this._eventInvokerMap[event]) {
-                if (target !== invoker.target || handler !== invoker.handler) {
-                    newEventMap.push(invoker);
-                }
-            }
-            this._eventInvokerMap[event] = newEventMap;
+            this._eventInvokerMap[event] = this._eventInvokerMap[event].filter((v, i, a) => {
+                return (target !== v.target || handler !== v.handler);
+            });
         }
     }
 
     // # 注册运行一次的事件
-    one(event: string, handler) {
+    one(event: string, handler: Function) {
+        if (handler == null) {
+            return;
+        }
+
         if (this._oneEventInvokerMap[event] == null) {
             this._oneEventInvokerMap[event] = [];
         }
@@ -45,19 +59,14 @@ export class EventDispatcher {
     // # 触发事件
     trigger(event: string, ...params) {
         // # trigger on event
-        if (this._eventInvokerMap[event] != null) {
-            for (let invoker of this._eventInvokerMap[event]) {
-                invoker.handler.call(invoker.target, ...params);
-            }
+        for (let invoker of this.getInvokerList(event)) {
+            invoker.handler.call(invoker.target, ...params);
         }
 
         // # trigger one event
-        if (this._oneEventInvokerMap[event] != null) {
-            for (let invoker of this._oneEventInvokerMap[event]) {
-                invoker(...params);
-            }
-
-            this._oneEventInvokerMap[event] = []
+        for (let invoker of this.getOneInvokerList(event)) {
+            invoker(...params);
         }
+        this._oneEventInvokerMap[event] = [];
     }
 }
